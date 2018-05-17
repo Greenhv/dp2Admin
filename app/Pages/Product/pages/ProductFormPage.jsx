18186a391bb527cb39b0e742e2a3925a82ea6c8b
jose chu <jchudb93@gmail.com>
2018-05-17 01:02:01 -0500

    product form page

--- app/Pages/Product/pages/ProductFormPage.jsx
+++ app/Pages/Product/pages/ProductFormPage.jsx
@@ -1,13 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FormGroup, FormControl, ControlLabel, Grid, Row, Col, Panel, Button, Label, Input } from 'react-bootstrap';
 
 const ProductFormPage = () => {
   return (
-    <div>
-      2
-    </div>
+    <Grid fluid>
+        <Row>
+          <Col lg={12}>
+            <Panel>
+              <div>
+                <form>
+                  <FormGroup>
+                    <ControlLabel>
+                      Nombre del producto
+                    </ControlLabel>
+                    <FormControl
+                      bsSize="small"
+                      type="text"
+                      placeholder="Nombre del producto"
+                    >
+                    </FormControl>
+                    <ControlLabel>
+                      Precio del producto
+                    </ControlLabel>
+                    <FormControl
+                      bsSize="small"
+                      type="text"
+                      placeholder="Precio del producto"
+                    >
+                    </FormControl>
+                    <ControlLabel>Marca</ControlLabel>
+                    <FormControl componentClass="select" placeholder="Marca">
+                      <option value="select">Marca 1</option>
+                      <option value="other">...</option>
+                    </FormControl>
+                    <ControlLabel>Categoria</ControlLabel>
+                    <FormControl componentClass="select" placeholder="Categoria">
+                      <option value="select">Categoria 1</option>
+                      <option value="other">...</option>
+                    </FormControl>
+                  </FormGroup>              
+                </form>             
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
   )
 }
+
 
 ProductFormPage.propTypes = {
 
