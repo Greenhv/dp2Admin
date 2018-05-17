3970a56753a4a83f982250b143f585b65effc68d
HerbertV <herbert@crehana.com>
2018-05-17 07:24:11 -0500

    Add redux form to page

--- app/Pages/Product/pages/ProductFormPage.jsx
+++ app/Pages/Product/pages/ProductFormPage.jsx
@@ -1,57 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormGroup, FormControl, ControlLabel, Grid, Row, Col, Panel, Button, Label, Input } from 'react-bootstrap';
+import { Form, reduxForm, Field } from 'redux-form';
 
-const ProductFormPage = () => {
-  return (
-    <Grid fluid>
-        <Row>
-          <Col lg={12}>
-            <Panel>
-              <div>
-                <form>
-                  <FormGroup>
-                    <ControlLabel>
-                      Nombre del producto
-                    </ControlLabel>
-                    <FormControl
-                      bsSize="small"
-                      type="text"
-                      placeholder="Nombre del producto"
-                    >
-                    </FormControl>
-                    <ControlLabel>
-                      Precio del producto
-                    </ControlLabel>
-                    <FormControl
-                      bsSize="small"
-                      type="text"
-                      placeholder="Precio del producto"
-                    >
-                    </FormControl>
-                    <ControlLabel>Marca</ControlLabel>
-                    <FormControl componentClass="select" placeholder="Marca">
-                      <option value="select">Marca 1</option>
-                      <option value="other">...</option>
-                    </FormControl>
-                    <ControlLabel>Categoria</ControlLabel>
-                    <FormControl componentClass="select" placeholder="Categoria">
-                      <option value="select">Categoria 1</option>
-                      <option value="other">...</option>
-                    </FormControl>
-                  </FormGroup>              
-                </form>             
-              </div>
-            </Panel>
-          </Col>
-        </Row>
-      </Grid>
-  )
-}
+const ProductFormPage = ({ match }) => (
+  <Grid fluid>
+      <Row>
+        <Col lg={12}>
+          <Panel>
+            <div>
+              <Form>
+                <FormGroup>
+                  <ControlLabel>
+                    Nombre del producto
+                  </ControlLabel>
+                  <FormControl
+                    bsSize="small"
+                    type="text"
+                    placeholder="Nombre del producto"
+                  >
+                  </FormControl>
+                  <ControlLabel>
+                    Precio del producto
+                  </ControlLabel>
+                  <FormControl
+                    bsSize="small"
+                    type="text"
+                    placeholder="Precio del producto"
+                  >
+                  </FormControl>
+                  <ControlLabel>Marca</ControlLabel>
+                  <FormControl componentClass="select" placeholder="Marca">
+                    <option value="select">Marca 1</option>
+                    <option value="other">...</option>
+                  </FormControl>
+                  <ControlLabel>Categoria</ControlLabel>
+                  <FormControl componentClass="select" placeholder="Categoria">
+                    <option value="select">Categoria 1</option>
+                    <option value="other">...</option>
+                  </FormControl>
+                </FormGroup>
+              </Form>
+            </div>
+          </Panel>
+        </Col>
+      </Row>
+    </Grid>
+);
 
 
 ProductFormPage.propTypes = {
 
 }
 
-export default ProductFormPage;
+export default reduxForm({
+  form: 'productForm',
+})(ProductFormPage);
