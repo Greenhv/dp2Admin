e66a3bd7ca506a98fb214defe178012071f4b425
José Chu <jchudb93@gmail.com>
2018-05-27 12:03:14 -0500

    store categories form

--- app/Pages/StoreCategory/pages/StoreCategoryFormPage.jsx
+++ app/Pages/StoreCategory/pages/StoreCategoryFormPage.jsx
@@ -16,7 +16,7 @@ import { reduxForm, Field } from "redux-form";
 
 import Select from "Shared/Select";
 import CustomInput from "Shared/Form/CustomInput";
-import { addStoreCategory } from "Modules/products";
+import { addStoreCategory } from "Modules/storeCategories";
 
 let StoreCategoryForm = props => {
   const { handleSubmit } = props;
@@ -24,20 +24,87 @@ let StoreCategoryForm = props => {
   <form onSubmit={handleSubmit} />;
 };
 
-const StoreCategoryFormPage = () => {
-  return (
-    <Grid fluid>
-      <Row>
-        <Col lg={12}>
-          <Panel>
-            <div>store_categories 2</div>
-          </Panel>
-        </Col>
-      </Row>
-    </Grid>
-  );
+const onStoreCategorySubmit = (values, dispatch) => {
+  if (Object.keys(values).length >= 2) {
+    fetch(`${process.env.API_BASE_URL}produts`, {
+      method: "POST",
+      body: JSON.stringify(values),
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        dispatch(addStoreCategory(data.storeCategory));
+        window.history.back();
+      });
+  }
 };
 
-StoreCategoryFormPage.propTypes = {};
+class StoreCategoryFormPage extends PureComponent {
+  componentDidMount() {
+    const element = ReactDOM.findDOMNode(this.form);
+    $(element).parsley();
+  }
 
-export default StoreCategoryFormPage;
+  goToStoreCategoriesPage = () => {
+    const { history } = this.props;
+    history.push("/categoria-de-tiendas");
+  };
+
+  render() {
+    const { history, handleSubmit } = this.props;
+    return(
+      <Grid fluid>
+      <Row>
+        <Col lg={12}>
+        <Panel>
+          <form onSubmit ={handleSubmit} noValidate ref={(node) => {this.form = node}}>
+          <Panel.Body>
+            <FormGroup>
+              <ControlLabel>Nombre de la categoría</ControlLabel>
+              <Field
+              name="name"
+              component={CustomInput}
+              type="text"
+              props = {{ placeholder: 'Nombre de la categoría', required: 'required'}}
+              />
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Descripción de la categoría</ControlLabel>
+              <Field
+              name="decription"
+              component={CustomInput}
+              type="textarea"
+              props={{ placeholder: 'Descripción del la categoría', required: 'required'}}
+              />
+            </FormGroup>
+          </Panel.Body>
+          <Panel.Footer>
+            <div className="form-footer">
+            <div className="form-button">
+            <Button onClick={this.goToStoreCategoriesPage}>Cancelar</Button>
+            </div>
+            <div className="form-button">
+              <Button type="submit">Crear</Button>
+            </div>
+            </div>
+          </Panel.Footer>
+          </form>
+        </Panel>
+        </Col>
+      </Row>
+      </Grid>
+    )
+  }
+}
+
+StoreCategoryFormPage .propTypes = {
+  history: shape({}).isRequired,
+}
+export default reduxForm({
+  form: 'productForm',
+  onSubmit: onStoreCategorySubmit,
+})(StoreCategoryFormPage)
+
+
