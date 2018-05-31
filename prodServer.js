b7c43f73e6243fec550984d7d6c46979eb930e6a
HerbertV <herbert@crehana.com>
2018-05-31 11:29:57 -0500

    Add cors to express server

--- prodServer.js
+++ prodServer.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
+var cors = require('cors');
 var app = express();
 var port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
