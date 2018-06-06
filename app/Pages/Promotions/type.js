5b6497d2c9f937a8018011e3d5669f9a505bdf74
José Chu <jchudb93@gmail.com>
2018-06-05 20:44:47 -0500

    DataTable witht promotions

--- app/Pages/Promotions/type.js
+++ app/Pages/Promotions/type.js
@@ -7,7 +7,7 @@ import {
 export const promotionType = shape({
     id: number,
     only_mobile: boolean,
-    inicial_date: string,
+    initial_date: string,
     final_date: string,
     value: string,
     store_id: number,
