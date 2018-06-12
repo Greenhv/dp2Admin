038affc312782f6433ab71487be34f8fa38fc608
HerbertV <herbert@crehana.com>
2018-06-12 09:45:30 -0500

    Change recursive namespace of in utils

--- app/Utils/objectToFormData.js
+++ app/Utils/objectToFormData.js
@@ -16,7 +16,7 @@ const objectToFormData = function(obj, form, namespace) {
         typeof obj[property] === 'object' &&
         !(obj[property] instanceof File)
       ) {
-        objectToFormData(obj[property], fd, property);
+        objectToFormData(obj[property], fd, formKey);
       } else {
         // if it's a string or a File object
         fd.append(formKey, obj[property]);
